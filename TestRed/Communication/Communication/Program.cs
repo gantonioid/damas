93fea5d5ec75bb8c2f0188b0389bce547daf23dd
Erick Castillo de la Garza <ecastillo550@gmail.com>
2014-11-07 19:11:10 -0600

    Cliente servidor para comunicación. Leer TODO para saber como va todo.

---
+++ TestRed/Communication/Communication/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Communication {
+    class Program {
+        static void Main(string[] args) {
+            IPAddress ipaddr = IPAddress.Parse("127.0.0.1");
+            TcpListener serverSocket = new TcpListener(ipaddr, 8888);
+            TcpClient clientSocket = default(TcpClient);
+            int counter = 0;
+
+            serverSocket.Start();
+            Console.WriteLine(" >> " + "Server Started");
+
+            counter = 0;
+            while (true) {
+                counter += 1;
+                clientSocket = serverSocket.AcceptTcpClient();
+                Console.WriteLine(" >> " + "Client No:" + Convert.ToString(counter) + " started!");
+                handleClinet client = new handleClinet();
+                client.startClient(clientSocket, Convert.ToString(counter));
+            }
+
+            clientSocket.Close();
+            serverSocket.Stop();
+            Console.WriteLine(" >> " + "exit");
+            Console.ReadLine();
+        }
+    }
+
+    //Class to handle each client request separatly
+    public class handleClinet {
+        TcpClient clientSocket;
+        string clNo;
+        private volatile bool stop;
+        Thread[] ctThread = new Thread[8];
+        
+
+        public void Stop() {
+             stop = true;
+        }
+
+        public void killthis() {
+
+        }
+
+        public void startClient(TcpClient inClientSocket, string clineNo) {
+            this.clientSocket = inClientSocket;
+            this.clNo = clineNo;
+            ctThread[Convert.ToInt32(clineNo)] = new Thread(new ThreadStart(doChat));
+            ctThread[Convert.ToInt32(clineNo)].Start();
+        }
+
+        public void doChat() {
+            int requestCount = 0;
+            byte[] bytesFrom = new byte[10024];
+            string dataFromClient = null;
+            Byte[] sendBytes = null;
+            string serverResponse = null;
+            string rCount = null;
+            requestCount = 0;
+
+            while (stop == false) {
+                try {
+                    requestCount = requestCount + 1;
+                    NetworkStream networkStream = clientSocket.GetStream();
+                    networkStream.Read(bytesFrom, 0, bytesFrom.Length);
+                    dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom);
+                    dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
+                    if (dataFromClient == "Close") {
+                        Console.WriteLine(" << " + "Close Client --- " + clNo);
+                        serverResponse = "Server to clinet(" + clNo + "):  Bye Bye! " + rCount;
+                        this.Stop();
+                    }
+                    else {
+                        Console.WriteLine(" >> " + "From client-" + clNo + dataFromClient);
+                        rCount = Convert.ToString(requestCount);
+                        serverResponse = "Server to clinet(" + clNo + ") " + rCount;
+                    }
+                    sendBytes = Encoding.ASCII.GetBytes(serverResponse);
+                    networkStream.Write(sendBytes, 0, sendBytes.Length);
+                    networkStream.Flush();
+                    Console.WriteLine(" >> " + serverResponse);
+                }
+                catch (Exception ex) {
+                    Console.WriteLine(" >> " + ex.ToString());
+                }
+            }
+        }
+    }
+}
---
+++ TestRed/CommunicationClient/CommClient/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+
+namespace CommClient {
+    public partial class Form1 : Form {
+        System.Net.Sockets.TcpClient clientSocket = new System.Net.Sockets.TcpClient();
+        NetworkStream serverStream; 
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            msg("Client Started");
+            clientSocket.Connect("127.0.0.1", 8888);
+            label1.Text = "Client Socket Program - Server Connected ...";
+        }
+
+        public void msg(string mesg) {
+            richTextBox1.Text = richTextBox1.Text + Environment.NewLine + " >> " + mesg + "\n";
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            NetworkStream serverStream = clientSocket.GetStream();
+            byte[] outStream = System.Text.Encoding.ASCII.GetBytes("Client$");
+            serverStream.Write(outStream, 0, outStream.Length);
+            serverStream.Flush();
+
+            byte[] inStream = new byte[10024];
+            serverStream.Read(inStream, 0, inStream.Length);
+            string returndata = System.Text.Encoding.ASCII.GetString(inStream);
+            msg("Data from Server : " + returndata);
+        }
+
+        private void Form1_Closing(object sender, System.ComponentModel.CancelEventArgs e) {
+            NetworkStream serverStream = clientSocket.GetStream();
+            byte[] outStream = System.Text.Encoding.ASCII.GetBytes("Close$");
+            serverStream.Write(outStream, 0, outStream.Length);
+            serverStream.Flush();
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            NetworkStream serverStream = clientSocket.GetStream();
+            byte[] outStream = System.Text.Encoding.ASCII.GetBytes("Close$");
+            serverStream.Write(outStream, 0, outStream.Length);
+            serverStream.Flush();
+
+            byte[] inStream = new byte[10024];
+            serverStream.Read(inStream, 0, inStream.Length);
+            string returndata = System.Text.Encoding.ASCII.GetString(inStream);
+            msg("Data from Server : " + returndata);
+            button1.Enabled = false;
+            button2.Enabled = false;
+        }
+    }
+}
